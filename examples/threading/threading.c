7b0b185e9c98e886d429e327346361f7edeaa348
Slobodan Pavlic <slobodan.pavlic@gmail.com>
2024-11-25 17:29:27 -0500

    Assignment 4 Part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,44 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data* ptr_data = (struct thread_data *)thread_param;
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(1000 * ptr_data->wait_to_obtain_ms);
+
+    if (0 != pthread_mutex_lock(ptr_data->mutex)) {
+        ptr_data->thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(1000 * ptr_data->wait_to_release_ms);
+
+    if (0 !=  pthread_mutex_unlock(ptr_data->mutex)) {
+        ptr_data->thread_complete_success = false;
+        return thread_param;
+    }
+
+    ptr_data->thread_complete_success = true;
+
     return thread_param;
 }
 
-
-bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data* ptr_data = malloc(sizeof(struct thread_data));
+
+    if (ptr_data == NULL) {
+        return false;
+    }
+
+    ptr_data->mutex = mutex;
+    ptr_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    ptr_data->wait_to_release_ms = wait_to_release_ms;
+    ptr_data->thread_complete_success = false;
+
+    if (0 != pthread_create(thread, NULL, threadfunc, ptr_data)) {
+        free(ptr_data);
+        return false;
+    }
+    return true;
 }
 
